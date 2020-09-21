ad03a099b50f945effcea73b72bbcdbd843e992a
luck7710 <lucky.dongang@gmail.com>
2020-09-21 19:49:30 +0200

    added voice recognition - Vanilla search

--- projects/vanilla-search/src/app/search-form/search-form.component.ts
+++ projects/vanilla-search/src/app/search-form/search-form.component.ts
@@ -1,25 +1,46 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl } from "@angular/forms";
+import { Component, NgZone, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
 import { SearchService } from '@sinequa/components/search';
 import { LoginService } from '@sinequa/core/login';
 import { AppService } from '@sinequa/core/app-utils';
 import { Subscription } from 'rxjs';
 import { FEATURES } from '../../config';
 
+declare const annyang: any;
+
 @Component({
   selector: 'app-search-form',
-  templateUrl: './search-form.component.html'
+  templateUrl: './search-form.component.html',
+  styleUrls: ['./search-form.component.scss']
 })
 export class SearchFormComponent implements OnInit, OnDestroy {
   searchControl: FormControl;
   form: FormGroup;
   autofocus = 0;
 
+  voiceActiveSectionDisabled: boolean = true;
+  voiceActiveSectionError: boolean = false;
+  voiceActiveSectionSuccess: boolean = false;
+  voiceActiveSectionListening: boolean = false;
+  voiceText: any;
+  private _searchSubscription: Subscription;
+
   constructor(
+    private ngZone: NgZone,
     public searchService: SearchService,
     public loginService: LoginService,
     private formBuilder: FormBuilder,
     public appService: AppService) {
+  }
+
+  /**
+   * Retrieve autocomplete sources, which include the standard
+   */
+  get autocompleteSources(): string[] {
+    if (this.appService.app && this.appService.app.data && this.appService.app.data.features) {
+      return <string[]>this.appService.app.data.features;
+    }
+    return FEATURES;
   }
 
   /**
@@ -37,9 +58,8 @@ export class SearchFormComponent implements OnInit, OnDestroy {
     });
   }
 
-  private _searchSubscription: Subscription;
-  ngOnDestroy(){
-    if(this._searchSubscription){
+  ngOnDestroy() {
+    if (this._searchSubscription) {
       this._searchSubscription.unsubscribe();
     }
   }
@@ -48,7 +68,7 @@ export class SearchFormComponent implements OnInit, OnDestroy {
    * Trigger a search query via the search service
    */
   search() {
-    if(this.loginService.complete){
+    if (this.loginService.complete) {
       this.searchService.clearQuery();
       this.searchService.query.text = this.searchControl.value || "";
       this.searchService.searchText("search");
@@ -60,23 +80,73 @@ export class SearchFormComponent implements OnInit, OnDestroy {
    * @param category
    */
   autocompleteIcon(category): string {
-    switch(category){
-      case "recent-document": return "far fa-file-alt fa-fw";
-      case "recent-query": return "fas fa-history fa-fw";
-      case "basket": return "fas fa-inbox fa-fw";
-      case "saved-query": return "far fa-save fa-fw";
+    switch (category) {
+      case "recent-document":
+        return "far fa-file-alt fa-fw";
+      case "recent-query":
+        return "fas fa-history fa-fw";
+      case "basket":
+        return "fas fa-inbox fa-fw";
+      case "saved-query":
+        return "far fa-save fa-fw";
     }
     return "far fa-lightbulb fa-fw";
   }
+  //  Part speech recognition
+  initializeVoiceRecognitionCallback(): void {
+    annyang.addCallback('error', (err) => {
+      if (err.error === 'network') {
+        this.voiceText = "Internet is require";
+        annyang.abort();
+        this.ngZone.run(() => this.voiceActiveSectionSuccess = true);
+      } else if (this.voiceText === undefined) {
+        this.ngZone.run(() => this.voiceActiveSectionError = true);
+        annyang.abort();
+      }
+    });
 
-  /**
-   * Retrieve autocomplete sources, which include the standard
-   */
-  get autocompleteSources(): string[] {
-    if(this.appService.app && this.appService.app.data && this.appService.app.data.features){
-      return <string[]> this.appService.app.data.features;
-    }
-    return FEATURES;
+    annyang.addCallback('soundstart', () => {
+      this.ngZone.run(() => this.voiceActiveSectionListening = true);
+    });
+
+    annyang.addCallback('end', () => {
+      if (this.voiceText === undefined) {
+        this.ngZone.run(() => this.voiceActiveSectionError = true);
+        annyang.abort();
+      }
+    });
+
+    annyang.addCallback('result', (userSaid) => {
+      this.ngZone.run(() => this.voiceActiveSectionError = false);
+
+      const queryText: any = userSaid[0];
+
+      annyang.abort();
+
+      this.voiceText = queryText;
+      this.form?.get('search')?.patchValue(queryText);
+
+      this.ngZone.run(() => this.voiceActiveSectionListening = false);
+      this.ngZone.run(() => this.voiceActiveSectionSuccess = true);
+    });
   }
 
+  startVoiceRecognition(): void {
+    this.voiceActiveSectionDisabled = false;
+    this.voiceActiveSectionError = false;
+    this.voiceActiveSectionSuccess = false;
+    this.voiceText = undefined;
+    if (annyang) {
+      const commands = {
+        'demo-annyang': () => {
+        }
+      };
+
+      annyang.addCommands(commands);
+
+      this.initializeVoiceRecognitionCallback();
+
+      annyang.start({ autoRestart: false });
+    }
+  }
 }
